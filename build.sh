2a551eb898ae5c80cf09544d55c2f5cbcb176278
mrbbbaixue <mrbbbaixue@outlook.com>
2024-12-03 11:14:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh;
 chmod +x dotnet-install.sh;
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet;
 ./dotnet/dotnet --version;
-./dotnet/dotnet run --project ./CSharpProject --working-dir=".\output"
+./dotnet/dotnet run --project ./CSharpProject --working-dir="./output"
