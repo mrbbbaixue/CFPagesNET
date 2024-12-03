f2e4abe76ef1246bfa671867a1f2db04f249dbc0
MrBBBaiXue <mrbbbaixue@outlook.com>
2024-12-03 16:11:48 +0800

    Remove cols in line end.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh;
-chmod +x dotnet-install.sh;
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet;
-./dotnet/dotnet --version;
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
 ./dotnet/dotnet run --project ./CSharpProject --working-dir="./output"
